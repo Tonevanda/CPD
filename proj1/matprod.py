3a281666a2aa186550c00843747b2fc3d1eaf929
Tonevanda <up202108863@up.pt>
2024-02-15 22:29:13 +0000

    Update matprod.py

--- proj1/matprod.py
+++ proj1/matprod.py
@@ -1,13 +1,25 @@
 import time
 
-# TODO - Change the code to not use numpy and reflect the one in the cpp file
 def OnMult():
     size = int(input("Enter the size of the matrix: "))
+
+
+    pha = [1 for _ in range(size*size)]
+    phb = [(i+1) for i in range(size) for j in range(size)]
+    phc = [0 for _ in range(size*size)]
+
     start = time.time()
-    matrix1 = [[1 for _ in range(size)] for _ in range(size)]
-    matrix2 = [[i+1 for _ in range(size)] for i in range(size)]
+
+    for i in range(size):
+        for j in range(size):
+            temp = 0
+            for k in range(size):
+                temp += pha[i*size+k] * phb[k*size+j]
+            phc[i*size+j] = temp
+
     end = time.time()
     print("Time taken: ", end - start)
+    print("Result matrix: ", phc[:10])
 
 # TODO - Use single list instead of list of lists
 def OnMultLine():
