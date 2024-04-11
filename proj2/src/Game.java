3b6e14f53c1be80fc4eee4de64c2d3b00dbdca3c
JoãoMiguelVieiraCardoso <95939460+JoaoMiguelVieiraCardoso@users.noreply.github.com>
2024-04-11 11:39:24 +0100

    added thread creating function

--- proj2/src/Game.java
+++ proj2/src/Game.java
@@ -1,15 +1,42 @@
 import java.net.Socket;
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game{
     private List<Socket> _userSockets;
 
+    List<Thread> threads = new ArrayList<>();
+
     public Game(int players, List<Socket> userSockets){
         this._userSockets = userSockets;
     }
 
-    public void start(){
-        // TODO: Implement game logic
+
+    public void start() throws InterruptedException {
+        for(int i = 0; i < 10; i++){
+            Thread thread = Thread.startVirtualThread(()-> {
+                try{
+
+                    Thread.sleep(Duration.ofSeconds(10));
+                }
+                catch(InterruptedException e){
+
+                }
+
+
+
+            });
+            threads.add(thread);
+
+        }
+
+        for(Thread thread : threads){
+            thread.join();
+
+        }
+
+
     }
 
     public static void main(String[] args) {
